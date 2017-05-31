7f67d65a4d0ed40874cb3b1feb3495e65020d1d1
alex2015 <alex2015@rambler.ru>
2017-05-31 15:26:47 +0300

    commit

--- BookingApp/Global.asax.cs
+++ BookingApp/Global.asax.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Data.Entity;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using BookingApp.Models;
 
 namespace BookingApp
 {
@@ -13,6 +11,8 @@ namespace BookingApp
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new BookDbInitializer());
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
---
+++ BookingApp/Models/BookDbInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+
+namespace BookingApp.Models
+{
+    public class BookDbInitializer : DropCreateDatabaseAlways<BookContext>
+    {
+        protected override void Seed(BookContext db)
+        {
+            db.Books.Add(new Book { Name = "Война и мир", Author = "Л. Толстой", Year = 1863 });
+            db.Books.Add(new Book { Name = "Отцы и дети", Author = "И. Тургенев", Year = 1862 });
+            db.Books.Add(new Book { Name = "Чайка", Author = "А. Чехов", Year = 1896 });
+
+            base.Seed(db);
+        }
+    }
+}
